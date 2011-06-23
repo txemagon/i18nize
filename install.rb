cc024f4e31ffe7cdb1cdc74716c717b9062b330d
txemagon / imasen <txema.gonz@gmail.com>
2011-06-23 15:27:31 +0200

    install.rb script

--- install.rb
+++ install.rb
@@ -4,10 +4,9 @@ source_root File.expand_path("../lib/generators/i18nize", __FILE__)
 copy_file("application_helper.rb", "app/helpers/application_i18n_helper.rb") 
 directory("templates", "lib/templates")
 
-info = <<INFO_F   
+puts <<INFO_F   
 Remember to add support for directories in your config files.
 
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**',     '*.{rb,yml}')]
 INFO_F
 
-puts info
