5dbd760a3007ebc86acdbf8804f2eb880982b1ff
txemagon / imasen <txema.gonz@gmail.com>
2011-06-23 15:16:57 +0200

    install script

--- install.rb
+++ install.rb
@@ -2,7 +2,7 @@ source_root File.expand_path("../lib/templates", __FILE__)
 source_root File.expand_path("../lib/generators/i18nize", __FILE__)
 
 copy_file("application_helper.rb", "app/helpers/application_i18n_helper.rb") 
-directory("templates", "lib/templates"
+directory("templates", "lib/templates")
 
 info = <<INFO_F   
 Remember to add support for directories in your config files.
